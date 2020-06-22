992ddcbc9d43d53c967dd674c8491d5a11944f2a
aravind rajeev <aravindvrajeev@gmail.com>
2020-06-22 15:21:55 +0530

    Updates

---
+++ precache-manifest.b2d174a66740d8ad0da104482192b09d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6355af5ebb5015c374",
+    "url": "/roboFriends/static/css/main.686c2f5c.chunk.css"
+  },
+  {
+    "revision": "0a6355af5ebb5015c374",
+    "url": "/roboFriends/static/js/main.cb23bea7.chunk.js"
+  },
+  {
+    "revision": "0d6209f39b04744aca3d",
+    "url": "/roboFriends/static/js/runtime~main.0e8c0065.js"
+  },
+  {
+    "revision": "34c9467f4a3ac949b1bc",
+    "url": "/roboFriends/static/css/2.79c700e7.chunk.css"
+  },
+  {
+    "revision": "34c9467f4a3ac949b1bc",
+    "url": "/roboFriends/static/js/2.b98a55d3.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "073b8a78313292cd5a33daddb8ed7760",
+    "url": "/roboFriends/index.html"
+  }
+];
